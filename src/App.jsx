3744faab393674a156af85bb60d129a7fd4ff7ea
Lukas <lukas.ancikevicius@gmail.com>
2021-08-11 11:58:25 +0300

    getting counter id and op value i n handlEcount

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,28 @@ function App() {
     { id: 4, value: 4 },
   ];
   const [counters, setCounters] = useState(counterData);
+
+  const handleCount = (id, operation) => {
+    console.log('counter with id,', id, 'operation', operation);
+    // const difference = special ? 2 : 1;
+    // const countFix = isNaN(count) ? 0 : count;
+
+    // if (operation === '-') return setCount(countFix - difference);
+    // if (operation === '+') return setCount(countFix + difference);
+    // if (operation === 'rs')
+    //   return special ? setCount('start over') : setCount(0);
+  };
+
   return (
     <div className="App">
       <div className="d-flex">
         {counters.map((c) => (
-          <Counter item={c.value} key={c.id} isSpecial={c.isSpecial} />
+          <Counter
+            onHandleCount={handleCount}
+            item={c}
+            key={c.id}
+            isSpecial={c.isSpecial}
+          />
         ))}
       </div>
       <button>ResetAll</button>
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,39 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-export default function Counter({ isSpecial: special, item }) {
-  //   const user = {
-  //     name: "mike",
-  //     age: 88,
-  //   };
-
-  //   const {name, age} = user // sukuria ta pati kas butu // const name = user.name
-
-  const [count, setCount] = useState(0);
-  const handleCount = (operation) => {
-    const difference = special ? 2 : 1;
-    const countFix = isNaN(count) ? 0 : count;
-
-    if (operation === '-') return setCount(countFix - difference);
-    if (operation === '+') return setCount(countFix + difference);
-    if (operation === 'rs')
-      return special ? setCount('start over') : setCount(0);
-  };
-
+// contorlled component
+export default function Counter({
+  onHandleCount: handleCount,
+  isSpecial: special,
+  item,
+}) {
   const handleDisabled = () => {
-    if (special && count === 0) return true;
-    if (count === 'start over') return true;
+    // if (special && count === 0) return true;
+    // if (count === 'start over') return true;
+    return false;
   };
   return (
     <div style={{ width: '25%' }}>
       <h2>{special && 'Special '}Counter</h2>
-      <h2>{item}</h2>
+      <h2>{item.value}</h2>
       <div>
-        <button onClick={() => handleCount('+')}>{special && '2'}+</button>
-        <button disabled={handleDisabled()} onClick={() => handleCount('-')}>
+        <button onClick={() => handleCount(item.id, '+')}>
+          {special && '2'}+
+        </button>
+        <button
+          disabled={handleDisabled()}
+          onClick={() => handleCount(item.id, '-')}
+        >
           {special && '2'}-
         </button>
       </div>
-      <button onClick={() => handleCount('rs')}>reset</button>
+      <button onClick={() => handleCount(item.id, 'rs')}>reset</button>
     </div>
   );
 }
