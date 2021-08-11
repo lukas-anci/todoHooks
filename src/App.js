e80e172903bf3f1c2e6d7dfb2b86462a9e1cde7f
Lukas <lukas.ancikevicius@gmail.com>
2021-08-11 10:58:49 +0300

    added special functionatlity

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
+
 import './App.css';
 import Counter from './components/Counter';
 
 function App() {
+  const counterData = [
+    { id: 1, value: 0 },
+    { id: 2, value: 0 },
+  ];
+  const [counters, setCounters] = useState(counterData);
   return (
     <div className="App">
-      <Counter />
+      <div className="d-flex">
+        <Counter />
+        <Counter />
+        <Counter special />
+      </div>
+      <button>ResetAll</button>
     </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Counter = () => {
-  return <div>lols</div>;
-};
+export default function Counter({ special }) {
+  //   const user = {
+  //     name: "mike",
+  //     age: 88,
+  //   };
 
-export default Counter;
+  //   const {name, age} = user // sukuria ta pati kas butu // const name = user.name
+
+  const [count, setCount] = useState(0);
+  const handleCount = (operation) => {
+    const difference = special ? 2 : 1;
+    const countFix = isNaN(count) ? 0 : count;
+
+    if (operation === '-') return setCount(countFix - difference);
+    if (operation === '+') return setCount(countFix + difference);
+    if (operation === 'rs')
+      return special ? setCount('start over') : setCount(0);
+  };
+  return (
+    <div style={{ width: '25%' }}>
+      <h2>{special && 'Special '}Counter</h2>
+      <h2>{count}</h2>
+      <div>
+        <button onClick={() => handleCount('+')}>{special && '2'}+</button>
+        <button onClick={() => handleCount('-')}>{special && '2'}-</button>
+      </div>
+      <button onClick={() => handleCount('rs')}>reset</button>
+    </div>
+  );
+}
